27273b726fdef4471678224e04c097670753266c
nicolas-d-torres <nicolastorres55@gmail.com>
2014-07-05 20:28:15 +0200

    CreateNewEventActivity layout init

--- app/src/main/java/gtsarandum/syncc/CreateNewCalendarEventActivity.java
+++ app/src/main/java/gtsarandum/syncc/CreateNewCalendarEventActivity.java
@@ -4,35 +4,114 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class CreateNewCalendarEventActivity extends Activity {
 
-    ScrollView scrollView;
-    EditText title;//event_title_edit_text
-    EditText location;//event_location_edit_text
-    TextView datePicker;//from_date_picker
-    //from_time_picker
-    //to_date_picker
-    //to_time_picker
-    //all_day_check
-    //repetition_spinner
-    //reminder_spinner
-    //description_edit_text
+    //elements inside the view
+    private ScrollView scrollView;
+    private EditText eventTitleEditText;                //event_title_edit_text
+    private EditText eventLocationEditText;             //event_location_edit_text
+    private TextView fromDatePicker;                    //from_date_picker - onclicklistener and text
+    private TextView fromTimePicker;                    //from_time_picker - onclicklistener and text
+    private TextView toDatePicker;                      //to_date_picker - onclicklistener and text
+    private TextView toTimePicker;                      //to_time_picker - onclicklistener and text
+    private CheckBox allDayCheck;                       //all_day_check - onstatechangedlistener
+    private Spinner repetitionSpinner;                  //repetition_spinner - fill with items through adapter
+    private Spinner reminderSpinner;                    //reminder_spinner - fill with items through adapter
+    private EditText descriptionEditText;               //description_edit_text
+
+    //necessary variables for view elements
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_calendar_event);
+        scrollView=(ScrollView)getLayoutInflater().inflate(R.layout.activity_create_new_calendar_event,null,false);
+        setContentView(scrollView);
+
+        init();
     }
+    private void init(){
+        //get all the views!
+        eventTitleEditText=(EditText) scrollView.findViewById(R.id.event_title_edit_text);
+        eventLocationEditText=(EditText) scrollView.findViewById(R.id.event_location_edit_text);
+        fromDatePicker=(TextView) scrollView.findViewById(R.id.from_date_picker);
+        fromTimePicker=(TextView) scrollView.findViewById(R.id.from_time_picker);
+        toDatePicker=(TextView) scrollView.findViewById(R.id.to_date_picker);
+        toTimePicker=(TextView) scrollView.findViewById(R.id.to_time_picker);
+        allDayCheck=(CheckBox) scrollView.findViewById(R.id.all_day_check);
+        repetitionSpinner=(Spinner) scrollView.findViewById(R.id.repetition_spinner);
+        reminderSpinner=(Spinner) scrollView.findViewById(R.id.reminder_spinner);
+        descriptionEditText=(EditText) scrollView.findViewById(R.id.description_edit_text);
 
-    @Override
-    public void onStart(){
-        super.onStart();
+        //adapter for spinners
+        reminderSpinner.setAdapter(ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.reminder_options, R.layout.custom_spinner_item));
 
-        scrollView=(ScrollView)findViewById(R.id.scrollview);
+        repetitionSpinner.setAdapter(ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.repetition_options, R.layout.custom_spinner_item));
+
+        //onItemSelected for spinners
+        reminderSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //change value
+            }
+        });
+
+        repetitionSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //chane value
+            }
+        });
+
+        //onClickListener for TextViews
+        fromDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open datepicker and change text in TextView
+            }
+        });
+
+        fromTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open timepicker and change text in TextView
+            }
+        });
+
+        toDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open datepicker and chane text in TextView
+            }
+        });
+
+        toTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open timepicker and change text in TextView
+            }
+        });
+
+        //set listener for checkbox
+        allDayCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //set value for dedicated boolean
+            }
+        });
     }
 
     @Override
