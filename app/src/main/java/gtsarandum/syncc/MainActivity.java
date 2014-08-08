473336d47ecf74a7494143a88a4e87eae0d23693
Torres <nicolastorres55@gmail.com>
2014-08-08 16:52:43 +0200

    ...

--- app/src/main/java/gtsarandum/syncc/MainActivity.java
+++ app/src/main/java/gtsarandum/syncc/MainActivity.java
@@ -8,16 +8,12 @@ import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
