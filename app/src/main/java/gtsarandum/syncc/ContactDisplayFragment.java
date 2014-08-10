f0688cc40424c41f3a98dff3245bbd3f42366990
Torres <nicolastorres55@gmail.com>
2014-08-10 03:22:15 +0200

    Floating Action Button

--- app/src/main/java/gtsarandum/syncc/ContactDisplayFragment.java
+++ app/src/main/java/gtsarandum/syncc/ContactDisplayFragment.java
@@ -7,8 +7,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class ContactDisplayFragment extends Fragment {
+
+    //attr
+    private RelativeLayout relativeLayout;
 
     public ContactDisplayFragment() {
         // Required empty public constructor
@@ -27,7 +31,14 @@ public class ContactDisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_display, container, false);
+        relativeLayout=(RelativeLayout) inflater.inflate(R.layout.fragment_contact_display, container, false);
+        return relativeLayout;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
     }
 
 
