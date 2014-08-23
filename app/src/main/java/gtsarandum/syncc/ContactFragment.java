d4f1733e3a70d6a1b31177b8358c42a7a64763a9
Torres <nicolastorres55@gmail.com>
2014-08-23 15:30:14 +0200

    more vars in SynccContact - TODO: find out how to retrieve emails

--- app/src/main/java/gtsarandum/syncc/ContactFragment.java
+++ app/src/main/java/gtsarandum/syncc/ContactFragment.java
@@ -21,6 +21,7 @@ public class ContactFragment extends ListFragment {
     private ContactAdapter contactAdapter;
     private customContactClickListener listener;
     private ArrayList<SynccContact> contacts;
+    private Cursor contactCursor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,43 +29,38 @@ public class ContactFragment extends ListFragment {
 
         contactAdapter=new ContactAdapter(getActivity().getApplicationContext());
         contacts=new ArrayList<SynccContact>();
-
-        fillContactList();
-
-        //setListAdapter
-        setListAdapter(contactAdapter);
-
-        fillView();
-    }
-
-    private void fillContactList(){
-
-    }
-
-    private void fillView(){
-        Cursor cursor=getActivity().getContentResolver().query(
+        contactCursor=getActivity().getContentResolver().query(
                 ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 new String[]{
-                    ContactsContract.CommonDataKinds.Phone._ID,//0
-                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,//1
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID,//2
-                    ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER,//3
-                    ContactsContract.CommonDataKinds.Phone.NUMBER,//4
-                    ContactsContract.Contacts.PHOTO_ID,//5
-                    ContactsContract.Contacts.PHOTO_THUMBNAIL_URI,//6
-                    ContactsContract.Contacts.PHOTO_URI},//7
+                        ContactsContract.CommonDataKinds.Phone._ID,//0
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,//1
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID,//2
+                        ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER,//3
+                        ContactsContract.CommonDataKinds.Phone.NUMBER,//4
+                        ContactsContract.Contacts.PHOTO_ID,//5
+                        ContactsContract.Contacts.PHOTO_THUMBNAIL_URI,//6
+                        ContactsContract.Contacts.PHOTO_URI
+                },
                 null,
                 null,
                 ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
 
+        //setListAdapter
+        setListAdapter(contactAdapter);
+
+        fillViewAndList();
+    }
+
+
+    private void fillViewAndList(){
         char cmp=' ';
         String name;
         String id;
 
-        if (cursor!=null && cursor.moveToFirst()){
-            for (int i=0;i<cursor.getCount();i++){
-                name=cursor.getString(1);
-                id=cursor.getString(0);
+        if (contactCursor!=null && contactCursor.moveToFirst()){
+            for (int i=0;i<contactCursor.getCount();i++){
+                name=contactCursor.getString(1);
+                id=contactCursor.getString(0);
                 if (cmp<name.charAt(0)){
                     cmp=name.charAt(0);
                     contactAdapter.addSectionHeaderItem(String.valueOf(cmp), null);
@@ -72,21 +68,14 @@ public class ContactFragment extends ListFragment {
 
                 contactAdapter.addItem(name,id);
 
-                cursor.moveToNext();
+                //add contact to list
+
+
+                contactCursor.moveToNext();
             }
         }
 
     }
-/*
-    private Bitmap loadContactPhoto(ContentResolver contentResolver,long id){
-        Uri uri= ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
-        InputStream inputStream= ContactsContract.Contacts.openContactPhotoInputStream(contentResolver,uri);
-        if(inputStream==null){
-            return null;
-        }
-
-        return BitmapFactory.decodeStream(inputStream);
-    }*/
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
