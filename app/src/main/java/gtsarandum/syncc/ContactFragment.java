8ab764e96ec99533e722b3b028d2dd8aef083b5a
nicolas-d-torres <nicolastorres55@gmail.com>
2014-09-16 17:14:46 +0200

    attempt at solution for filtering out dividers to get correct contact...Failed

--- app/src/main/java/gtsarandum/syncc/ContactFragment.java
+++ app/src/main/java/gtsarandum/syncc/ContactFragment.java
@@ -10,6 +10,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -119,6 +120,10 @@ public class ContactFragment extends ListFragment {
         if (listener!=null){
             //TODO : filter out dividers... -.-
 
+            //remove number of dividers from position
+            TextView textView=(TextView) v.findViewById(R.id.text);
+            position=position-getNumbersToRemove(textView.getText().charAt(0))-1;
+
             listener.onContactClick(l,v,position,id,contacts.get(position));
         }
     }
@@ -138,4 +143,61 @@ public class ContactFragment extends ListFragment {
         void onContactLongClick(AdapterView<?> adapterView, View view, int i, long l);
     }
 
+    private int getNumbersToRemove(char c){
+        switch (c){
+            case 'a':
+                return 1;
+            case 'b':
+                return 2;
+            case 'c':
+                return 3;
+            case 'd':
+                return 4;
+            case 'e':
+                return 5;
+            case 'f':
+                return 6;
+            case 'g':
+                return 7;
+            case 'h':
+                return 8;
+            case 'i':
+                return 9;
+            case 'j':
+                return 10;
+            case 'k':
+                return 11;
+            case 'l':
+                return 12;
+            case 'm':
+                return 13;
+            case 'n':
+                return 14;
+            case 'o':
+                return 15;
+            case 'p':
+                return 16;
+            case 'q':
+                return 17;
+            case 'r':
+                return 18;
+            case 's':
+                return 19;
+            case 't':
+                return 20;
+            case 'u':
+                return 21;
+            case 'v':
+                return 22;
+            case 'w':
+                return 23;
+            case 'x':
+                return 24;
+            case 'y':
+                return 25;
+            case 'z':
+                return 26;
+            default:return 0;
+        }
+    }
 }
