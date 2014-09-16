7c16fa41a49f2b74150d8af73b89bae141104824
Torres <nicolastorres55@gmail.com>
2014-09-16 15:21:02 +0200

    unkown error when clicking contact...

--- app/src/main/java/gtsarandum/syncc/ContactDisplayFragment.java
+++ app/src/main/java/gtsarandum/syncc/ContactDisplayFragment.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 
@@ -19,6 +20,7 @@ public class ContactDisplayFragment extends Fragment {
 
     //attr
     private SynccContact synccContact;
+    private FrameLayout frameLayout;
 
     public ContactDisplayFragment() {
         // Required empty public constructor
@@ -34,7 +36,8 @@ public class ContactDisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_display, container, false);
+        frameLayout=(FrameLayout)inflater.inflate(R.layout.fragment_contact_display,container,false);
+        return frameLayout;
     }
 
     @Override
@@ -57,6 +60,7 @@ public class ContactDisplayFragment extends Fragment {
         );
 
         test(synccContact.getName());
+
     }
 
     private void test(CharSequence charSequence){
